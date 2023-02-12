c56a48b16597f7c083d64fcd8fa712c22c25ca3d
WooLeeHappy <118141462+WooLeeHappy@users.noreply.github.com>
2023-02-12 23:27:42 +0900

    chore : 연습단계

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -24,10 +27,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 300, 300);
-        Button button = new Button();
-        button.setText("엑셀파일 가져오기");
-        button.setOnAction(new EventHandler<ActionEvent>() {
+        VBox vBox = new VBox();
+        Scene page1 = new Scene(layout, 640, 480);
+        Scene page2 = new Scene(vBox, 400, 200);
+        Label label = new Label("먼저 변형할 엑셀 파일을 넣어주세요");
+        Button getExcel = new Button("파일 가져오기");
+        // 엑셀 파일 가져오는 부분
+        vBox.getChildren().addAll(getExcel, label);
+        getExcel.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 FileChooser fileChooser = new FileChooser();
@@ -60,11 +67,26 @@ public class Main extends Application {
                 }
             }
         });
-        layout.getChildren().add(button);
-        stage.setScene(scene);
+
+        //  엑셀 파일의 숫자 받아오는 부분
+        TextField input = new TextField();
+        Button submitButton = new Button();
+        submitButton.setText("금액지정");
+        layout.getChildren().add(submitButton);
+        submitButton.setOnAction(event -> getLowMoney(input.getText()));
+
+        //
+        stage.setScene(page1);
+        stage.setScene(page2);
         stage.setTitle("상플");
         stage.show();
     }
+
+    private String getLowMoney(String text) {
+        System.out.println(text);
+        return text;
+    }
+
     public void test() {
         System.out.println("잘 됬다");
     }
