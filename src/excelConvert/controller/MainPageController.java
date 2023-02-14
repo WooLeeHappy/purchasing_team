0993681f8406d2e9109876ad90ea710719b51f41
WooLeeHappy <118141462+WooLeeHappy@users.noreply.github.com>
2023-02-14 18:37:00 +0900

    feat : 옵션 가져오기 성공

--- src/excelConvert/controller/MainPageController.java
+++ src/excelConvert/controller/MainPageController.java
@@ -16,11 +16,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
-    FileChooser fileChooser = new FileChooser();
+
     @FXML
     private Button fileUpload1;
     @FXML
     private Label fileName;
+
+    private File file;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -47,9 +49,8 @@ public class MainPageController implements Initializable {
         Dragboard db = event.getDragboard();
         boolean success = false;
         if (db.hasFiles() && isExcelFile(db.getFiles().get(0))) {
-            File file = db.getFiles().get(0);
+            file = db.getFiles().get(0);
             fileName.setText(file.getName());
-            openFile(file);
             success = true;
         }
         event.setDropCompleted(success);
@@ -62,13 +63,15 @@ public class MainPageController implements Initializable {
     // 버튼 파일 업로드 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
     @FXML
     public void uploadFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel Files", "*.xlsx", "*.xls"));
-        File file = fileChooser.showOpenDialog(null);
+        file = fileChooser.showOpenDialog(null);
 
         if (file != null) {
-            openFile(file);
             fileName.setText(file.getName());
             System.out.println("파일업로드 성공");
+        } else {
+            System.out.println("파일업로드 실패");
         }
     }
     // 버튼 and 드래그한 파일 오픈하기  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
@@ -98,17 +101,39 @@ public class MainPageController implements Initializable {
     }
 
     // 토글 선택 관련 컨트롤   ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-    @FXML
-    private ToggleGroup upDown;
-    @FXML Button convert;
+    @FXML private ToggleGroup upDown;
+    @FXML private Button convert;
+    private String check;
+    @FXML private TextField amount;
 
     @FXML
     public void convert(ActionEvent event) {
         RadioButton selectedRadioButton = (RadioButton) upDown.getSelectedToggle();
         if(!(fileName.getText().equals("엑셀파일 이름이 나타납니다"))) {
             if(selectedRadioButton != null) {
-                String selectedText = selectedRadioButton.getText();
-                System.out.println(selectedText);
+                if(amount.getText() != null) {
+                    try {
+                        String amountStr = amount.getText();
+                        int amount = Integer.parseInt(amountStr);
+                        String selectedText = selectedRadioButton.getText();
+                        if(selectedText.equals("이상")) {
+                            check = "up";
+                        } else {
+                            check = "down";
+                        }
+                        converting(amount, check);
+                    } catch (NumberFormatException e) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, "금액을 입력해주세요!", ButtonType.OK);
+                        alert.showAndWait();
+                    }
+
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("변환 실패");
+                    alert.setContentText("금액을 설정해주세요!");
+                    alert.showAndWait();
+                }
+
             }  else {
                 Alert alert = new Alert(Alert.AlertType.WARNING);
                 alert.setTitle("변환 실패");
@@ -119,10 +144,21 @@ public class MainPageController implements Initializable {
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("변환 실패");
-            alert.setContentText("파일을 업로드해주세요!");
+            alert.setContentText("엑셀 파일을 업로드해주세요!");
             alert.showAndWait();
         }
     }
 
 
+    //  엑셀 파일 변환작업
+    public void converting(int amount ,String check) {
+
+        if(check.equals("up")) {
+            System.out.println("체크 컨버팅 up");
+            System.out.println(amount);
+        } else {
+            System.out.println("체크 컨버팅 down");
+            System.out.println(amount);
+        }
+    }
 }
