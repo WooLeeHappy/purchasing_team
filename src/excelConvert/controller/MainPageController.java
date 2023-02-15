5fbdb4aef52131ed80cf018ed06b618dbc268a5c
WooLeeHappy <118141462+WooLeeHappy@users.noreply.github.com>
2023-02-15 19:59:11 +0900

    chore : 기본 틀 초기화

--- src/excelConvert/controller/MainPageController.java
+++ src/excelConvert/controller/MainPageController.java
@@ -120,7 +120,7 @@ public class MainPageController implements Initializable {
                     XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
                     XSSFSheet sheet = workbook.getSheetAt(0);
 
-                    readExcelService.readExcel(sheet);
+                    readExcel(sheet);
 
 
                     excelFile.close();
@@ -144,26 +144,8 @@ public class MainPageController implements Initializable {
         }
     }
 
-    // 엑셀 정렬 메서드
+    // 엑셀 읽기 메서드
     public void readExcel(Sheet sheet) throws IOException {
-//        readExcelService.readExcel(sheet);
-
-        for (Row row : sheet) {
-            // 각각의 행에 존재하는 모든 열(cell)을 순회한다.
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-
-                // cell의 타입을 하고, 값을 가져온다.
-                switch (cell.getCellType()) {
-                    case NUMERIC ->
-                        //getNumericCellValue 메서드는 기본으로 double형 반환
-                            System.out.print((int) cell.getNumericCellValue() + "\t");
-                    case STRING -> System.out.print(cell.getStringCellValue() + "\t");
-                }
-            }
-            System.out.println("");
-        }
+        readExcelService.readExcel(sheet);
     }
 }
